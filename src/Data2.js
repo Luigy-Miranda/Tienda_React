d6b9064d1474f3aa0b34f31d347e0f997e871559
Luigy12-source <53915057+Luigy12-source@users.noreply.github.com>
2021-02-21 03:30:17 -0500

    s

---
+++ src/Data2.js
@@ -0,0 +1,117 @@
+const data = {
+  status: "success",
+  items: [
+    {
+      id: 1,
+      title: "Mochila de flores",
+      price: 20,
+      image: "https://i.pinimg.com/564x/24/19/25/24192519f220b909669ed433e5124fbb.jpg",
+      category: "Mochila chica",
+      img1: "https://i.pinimg.com/564x/24/19/25/24192519f220b909669ed433e5124fbb.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606321430&w=1000",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      title: "Mochila transparente",
+      price: 19,
+      image: "https://i.pinimg.com/564x/e3/1a/bf/e31abfe3ba7fa485a90e828b03faafc7.jpg",
+      category: "Mochila chica",
+      img1: "https://i.pinimg.com/564x/e3/1a/bf/e31abfe3ba7fa485a90e828b03faafc7.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606319825&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 3,
+      title: "Mochila negra con logo",
+      price: 25,
+      image: "https://i.pinimg.com/564x/6a/05/ef/6a05ef0701c4021a7d1d7650c5adcc2f.jpg",
+      category: "Mochila chico",
+      img1: "https://i.pinimg.com/564x/6a/05/ef/6a05ef0701c4021a7d1d7650c5adcc2f.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325289&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 4,
+      title: "Mochila estudiantil",
+      price: 23,
+      image: "https://i.pinimg.com/564x/eb/51/dd/eb51dda09a5aedd3ab0605ab89d0059d.jpg",
+      category: "Mochila chico",
+      img1: "https://i.pinimg.com/564x/eb/51/dd/eb51dda09a5aedd3ab0605ab89d0059d.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325338&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 5,
+      title: "Mochila de pucca",
+      price: 21,
+      image: "https://i.pinimg.com/564x/3c/96/9b/3c969be5edd5e892337be6d2db32e3e3.jpg",
+      category: "Mochila chica",
+      img1: " https://i.pinimg.com/564x/3c/96/9b/3c969be5edd5e892337be6d2db32e3e3.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606322330&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 6,
+      title: "Mochila clasica",
+      price: 22,
+      image: "https://i.pinimg.com/564x/02/fe/7e/02fe7ed59b95b4b0fa2d54efdf8ae311.jpg",
+      category: "Mochila chico",
+      img1: "https://i.pinimg.com/564x/02/fe/7e/02fe7ed59b95b4b0fa2d54efdf8ae311.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606321824&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 7,
+      title: "Mochila ploma - diseño clasico",
+      price: 23,
+      image: "https://i.pinimg.com/564x/cc/e0/9f/cce09fd5df674ca23c6cea89d94fe2f4.jpg",
+      category: "Mochila chico",
+      img1: "https://i.pinimg.com/564x/cc/e0/9f/cce09fd5df674ca23c6cea89d94fe2f4.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606320966&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 8,
+      title: "Mochila negra",
+      price: 22,
+      image: "https://i.pinimg.com/564x/93/9d/84/939d848c22fc02cd384cc33d9cbac189.jpg",
+      category: "Mochila chico",
+      img1: "https://i.pinimg.com/564x/93/9d/84/939d848c22fc02cd384cc33d9cbac189.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325265&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 9,
+      title: "Mochila -diseño strager things",
+      price: 25,
+      image: "https://i.pinimg.com/564x/07/b0/3e/07b03ec48a7f9c8ab1648ad392ef42d4.jpg",
+      category: "Mochila chica",
+      img1: "https://i.pinimg.com/564x/07/b0/3e/07b03ec48a7f9c8ab1648ad392ef42d4.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606323250&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 10,
+      title: "Mochila ploma - grafea",
+      price: 21,
+      image: "https://i.pinimg.com/564x/90/38/b4/9038b4288279a1e7a08360a70c585536.jpg",
+      category: "Mochila chica",
+      img1: "https://i.pinimg.com/564x/90/38/b4/9038b4288279a1e7a08360a70c585536.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1609445259&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 11,
+      title: "Mochila color piel",
+      price: 23,
+      image: "https://i.pinimg.com/564x/2a/eb/e2/2aebe25ede846f19fd9cbae7fb29feae.jpg",
+      category: "Mochila chica",
+      img1: "https://i.pinimg.com/564x/2a/eb/e2/2aebe25ede846f19fd9cbae7fb29feae.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1609438911&w=1000",
+      cantidad: 1
+    },
+  ],
+};
+
+export default data;
